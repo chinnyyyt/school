f96d52ba3c2a459f4fc782fd1365a7554709f95a
chinnyyyt <103641236+chinnyyyt@users.noreply.github.com>
2024-08-14 07:16:56 +0800

    Update DictionaryAttack.rb

--- portfolio/week6/DictionaryAttack.rb
+++ portfolio/week6/DictionaryAttack.rb
@@ -1,16 +1,40 @@
 #!/usr/bin/ruby
 require 'digest'
-#read the password hash
+
+# Define the target password hash
 passwordHash = "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5"
-#open the wordlist and repeat for each word
-IO.foreach("wordlist.txt") do | word |
-    word.chomp!
-    #hash the word
-    wordlistHash = Digest::SHA256.hexdigest(word)
-    puts("Trying password #{word}: #{wordlistHash}")
-    #if the hash is the same as the correct password's hash then we have cracked the password!
-    if(wordlistHash == passwordHash)
-        puts("Password has been cracked! It was #{word}")
-        return
-    end
+
+# Read the wordlist into an array
+wordlist = IO.readlines("wordlist.txt").map(&:chomp)
+
+# Method to generate permutations of a given length from the wordlist
+def generate_permutations(length, wordlist)
+  wordlist.repeated_permutation(length).each do |combination|
+    yield combination.join
+  end
 end
+
+# Method to attempt brute force attack using permutations
+def brute_force_attack(passwordHash, wordlist, max_length)
+  (1..max_length).each do |length|
+    generate_permutations(length, wordlist) do |password|
+      # Hash the password
+      passwordHashAttempt = Digest::SHA256.hexdigest(password)
+      puts("Trying password #{password}: #{passwordHashAttempt}")
+
+      # Check if the hash matches the target hash
+      if passwordHashAttempt == passwordHash
+        puts("Password has been cracked! It was #{password}")
+        return
+      end
+    end
+  end
+
+  puts("Password not found within the given length.")
+end
+
+# Set the maximum length of permutations to try
+max_length = 2 # Adjust this length as needed
+
+# Start the brute force attack
+brute_force_attack(passwordHash, wordlist, max_length)
