51013dd3f2dbff2de010857db81e1a7fe2917125
chinnyyyt <103641236+chinnyyyt@users.noreply.github.com>
2024-08-14 06:51:24 +0800

    Update foldercopier.sh

--- portfolio/week3/foldercopier.sh
+++ portfolio/week3/foldercopier.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
 
-read -p "type the name of the folder you would like to copy: " folderName
+read -p "Type the name of the folder you would like to copy: " folderName
 
-#if the name is a valid directory
-
+# If the name is a valid directory
 if [ -d "$folderName" ]; then
 
-     #copy it to a new location
+    # Copy it to a new location
+    read -p "Type the name of the destination folder: " newFolderName
 
-     read -p "type the name of the destination folder: " newFolderName
-
-     cp -r "$folderName" "$newFolderName"
+    cp -r "$folderName" "$newFolderName"
+    echo "Folder copied successfully to $newFolderName."
 
 else
 
-     #otherwise, print an error
-
-     echo "I couldn't find that folder"
-
+    # Otherwise, print an error
+    echo "I couldn't find that folder."
 fi
-					
